b21068d991c0dd8fba753d411e182a48f6f515c2
Jacinto <jacintoquimua7@hotmail.com>
2025-11-05 18:23:23 -0700

    ground truth

---
+++ divide21x/ground_truth/ground_truth.py
@@ -0,0 +1,13 @@
+
+
+class GroundTruth():
+    '''
+    this is the ground truth aka optimal solver of the game Divide21.
+    '''
+    def __init__(self, state):
+        self.action = None
+        
+    def get_action(self):
+        return self.action
+    
+    
