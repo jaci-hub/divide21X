4c9fc164eab0cc9bd26d0d71c40c20038889c8f6
Jacinto <jacintoquimua7@hotmail.com>
2025-11-15 15:51:18 -0700

    add leaderboards

--- divide21x/envs/divide21x_main.py
+++ divide21x/envs/divide21x_main.py
@@ -1,3 +1,4 @@
+import csv
 import gymnasium as gym
 from gymnasium import spaces
 import divide21env
@@ -5,11 +6,12 @@ import json
 import os
 from divide21x.grading.grader import Grader
 from divide21x.utils.logger import EpisodeLogger
-from divide21x.utils.util import get_utc_date, get_utc_day, get_utc_hour
+from divide21x.utils.util import get_llm_registry, get_utc_date, get_utc_day, get_utc_hour
 
 
 BASE_DIR='./divide21x/envs/logs'
 RESULTS_DIR = './divide21x/results'
+LEADERBOARDS_DIR = './divide21x/leaderboards'
 # categories
 ENVIRONMENT = 'environment'
 RUN = 'run'
@@ -59,19 +61,41 @@ if __name__ == "__main__":
     file_name = day + '.json'
     results_path = os.path.join(RESULTS_DIR, date[:7])
     file = os.path.join(results_path, file_name)
+    # for leaderboard
+    leaderboard_file_name = day + '.csv'
+    leaderboards_path = os.path.join(LEADERBOARDS_DIR, date[:7])
+    os.makedirs(leaderboards_path, exist_ok=True)
+    leaderboard_file = os.path.join(leaderboards_path, leaderboard_file_name)
     
     if os.path.exists(file):
         with open(file, 'r') as f:
             data = json.load(f)
         
     if data:
+        leaderboard_data = []
         for key, value in data.items():
             divide21x = Divide21X(state=value[ANSWER])
             divide21x.start()
             # add result
             value[RESULT] = divide21x.get_result()
-    
+            # for leaderboard
+            provider = None
+            registry = get_llm_registry()
+            for entry in registry:
+                if entry['alias'] == key:
+                    provider = entry['provider']
+                    break
+            leaderboard_data.append([key, provider, value[RESULT]])
+        
         # update the file
         with open(file, 'w') as f:
             json.dump(data, f, indent=4)
-    
+        
+        # sort leaderboard data by result descending
+        leaderboard_data.sort(key=lambda x: x[2],  reverse=True)
+        # create leaderboard csv file
+        with open(leaderboard_file, 'w') as f:
+            header = ["Model", "Provider", "Score"]
+            leaderboard_data.insert(0, header)
+            writer = csv.writer(f)
+            writer.writerows(leaderboard_data)
